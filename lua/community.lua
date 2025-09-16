1f4f5d1e983bce5d2eeca55480bc09249a542449
Keisuke Umeno <9renpoto@gmail.com>
2025-09-16 19:48:43 +0900

    chore: update plugins

--- lua/community.lua
+++ lua/community.lua
@@ -8,11 +8,14 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.dracula-nvim" },
+  { import = "astrocommunity.pack.astro" },
   { import = "astrocommunity.pack.biome" },
+  { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.golangci-lint" },
+  { import = "astrocommunity.pack.ruby" },
   { import = "astrocommunity.pack.sql" },
-  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.typescript-deno" },
   -- import/override with your plugins folder
 }
