46f8a5ec7d01f231260981d3eee60360b1119bbb
Keisuke Umeno <9renpoto@gmail.com>
2025-09-26 13:30:54 +0900

    feat: configure stylua and fix related issues
    - Add `call_parentheses` and `sort_requires` to `.stylua.toml`.
    - Rename `stylelua.yml` to `stylua.yml` and fix its content.
    - Fix `lefthook.yml` to use the correct `stylua` command.
    - Format `lua/plugins/astrolsp.lua` with the new stylua settings.

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -75,7 +75,7 @@ return {
           desc = "Refresh codelens (buffer)",
           callback = function(args)
             if require("astrolsp").config.features.codelens then
-              vim.lsp.codelens.refresh { bufnr = args.buf }
+              vim.lsp.codelens.refresh({ bufnr = args.buf })
             end
           end,
         },
